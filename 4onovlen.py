1ed338da09a89328f24c5174086d823212d09b3b
Andrej-153 <andrejmaslov11143@gmail.com>
2024-07-02 07:50:01 +0300

    Add files via upload
    измененное дом задание , которое исчезло

---
+++ 4onovlen.py
@@ -0,0 +1,20 @@
+print('1st program')
+a=9
+b=5
+c=(a*b)
+print(c)
+print('2nd programm')
+print(9.99>9.98 and 1000!=1000.01)
+print('3d_programm')
+a=1234
+b=5678
+print(int(a%1000//10+b%5000//10))
+print('4programm')
+a=13.42
+b=42.13
+print(a//1)
+print(int(b%42*100))
+print(a//1==int(b%42*100))
+
+
+
